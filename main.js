ba65b05b5bb115f1a7a9759fecf06d96458b10e8
Saba Shavidze <shavidze>
2019-04-18 11:16:52 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,40 @@
+
+// Fade OUT
+// var typed = new Typed(".typed", {
+//     strings: ['Some <i>strings</i> with', 'Some <strong>HTML</strong>', 'Chars &times; &copy;'],
+//     typeSpeed: 0,
+//     backSpeed: 0,
+//     fadeOut: true,
+//     loop: true
+// })
+
+//fade axalze
+// var typed = new Typed(".typed", {
+//     strings: ['Some <i>strings</i> with', 'Some <strong>HTML</strong>', 'Chars &times; &copy;'],
+//     typeSpeed: 0,
+//     backSpeed: 0,
+//     fadeOut: true,
+//     loop: true
+// })
+
+
+//placeholderis cvla
+// var typed = new Typed(".typed", {
+//     strings: ['Some strings without', 'Some HTML', 'Chars'],
+//     typeSpeed: 0,
+//     backSpeed: 0,
+//     attr: 'placeholder',
+//     bindInputFocusEvents: true,
+//     loop: true
+// })
+
+var typed = new Typed(".typed", {
+    strings: ['1 Some <i>strings</i> with', '2 Some <strong>HTML</strong>', '3 Chars &times; &copy;'],
+    typeSpeed: 0,
+    backSpeed: 0,
+    attr: 'placeholder',
+    cursorChar: '_',
+    shuffle: true,
+    smartBackspace: false,
+    loop: true
+})
